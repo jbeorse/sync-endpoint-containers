55483ce12918e067fafd827ecaf378d4b4c13a10
linl33 <linlee1997@gmail.com>
2017-07-17 03:37:27 -0700

    update image to use Docker Configs/Secrets

--- sqlserver_endpoint/init.sh
+++ sqlserver_endpoint/init.sh
@@ -1,72 +1,10 @@
-#!/bin/bash
+#!/bin/bash -eu
 
-# Set path variables
-BASEPATH="/tmp"
-WARPATH="$BASEPATH/ODKAggregate.war"
-ROOTPATH="$BASEPATH/ODKAggregate"
-SETTINGSNAME="odk-sqlserver-settings-latest"
-SETTINGSPATH="$BASEPATH/$SETTINGSNAME"
-SETTINGSJARPATH="$ROOTPATH/WEB-INF/lib/$SETTINGSNAME.jar"
-SECURITYPATH="$SETTINGSPATH/security.properties"
-JDBCPATH="$SETTINGSPATH/jdbc.properties"
-RESULTPATH="/usr/local/tomcat/webapps/ROOT"
+set -o pipefail
 
-# Sed replacement strings for properties
-## Server hostname
-SERVERURL_PROP="s/security\.server\.hostname=.*/security\.server\.hostname=$SERVER_URL/g"
-## Server port
-SERVERPORT_PROP="s/security\.server\.port=.*/security\.server\.port=${SERVER_PORT:="8080"}/g"
-## Secure server port
-SERVERSECUREPORT_PROP="s/security\.server\.securePort=.*/security\.server\.securePort=${SERVER_SECURE_PORT:="8443"}/g"
-## JDBC URL
-JDBCURL_PROP="s/jdbc\.url=.*/jdbc\.url=jdbc:sqlserver:\/\/$DB_URL;database=$DB_NAME;user=${DB_USERNAME:="odk_user"};password=$DB_PASSWORD;encrypt=true;trustServerCertificate=${TRUST_SERVER_CERT:="false"};hostNameInCertificate=${SERVER_HOST_IN_CERT:="*.database.windows.net"};loginTimeout=30;authentication=ActiveDirectoryPassword/g"
-## Database Schema
-DBSCHEMA_PROP="s/jdbc\.schema=.*/jdbc\.schema=${DB_SCHEMA:="odk_prod"}/g"
+if [ -f /org.opendatakit.sync.ldapcert ]; then
+    echo "Adding LDAP CA Certificate"
+    keytool -noprompt -import -trustcacerts -alias ldapcert -keystore "/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security/cacerts" -file /org.opendatakit.sync.ldapcert -storepass changeit
+fi
 
-## LDAP Domain L1
-LDAP_DOMAIN_L1_PROP="s/security\.server\.ldapDomainDClevel1=.*/security\.server\.ldapDomainDClevel1=$LDAP_DOMAIN_L1/g"
-## LDAP Domain L2
-LDAP_DOMAIN_L2_PROP="s/security\.server\.ldapDomainDClevel2=.*/security\.server\.ldapDomainDClevel2=$LDAP_DOMAIN_L2/g"
-## LDAP Query Username
-LDAP_USERNAME_PROP="s/security\.server\.ldapQueryUsername=.*/security\.server\.ldapQueryUsername=$LDAP_USERNAME/g"
-## LDAP Query Password
-LDAP_PASSWORD_PROP="s/security\.server\.ldapQueryPassword=.*/security\.server\.ldapQueryPassword=$LDAP_PASSWORD/g"
-## Group prefix
-GROUP_PREFIX_PROP="s/security\.server\.groupPrefix=.*/security\.server\.groupPrefix=$GROUP_PREFIX/g"
-## Channel Type
-CHANNEL_TYPE_PROP="s/security\.server\.channelType=.*/security\.server\.channelType=$CHANNEL_TYPE/g"
-## Secure Channel Type
-SECURE_CHANNEL_TYPE_PROP="s/security\.server\.secureChannelType=.*/security\.server\.secureChannelType=$SECURE_CHANNEL_TYPE/g"
-## Realm String
-REALM_STRING_PROP="s/security\.server\.realm\.realmString=.*/security\.server\.realm\.realmString=$REALM_STRING/g"
-
-
-# Unzip war and jar files
-unzip $WARPATH -d $ROOTPATH
-unzip $SETTINGSJARPATH -d $SETTINGSPATH
-rm $SETTINGSJARPATH
-
-# Update the settings with the configured values
-sed -i $SERVERURL_PROP $SECURITYPATH
-sed -i $SERVERPORT_PROP $SECURITYPATH
-sed -i $SERVERSECUREPORT_PROP $SECURITYPATH
-sed -i $LDAP_DOMAIN_L1_PROP $SECURITYPATH
-sed -i $LDAP_DOMAIN_L2_PROP $SECURITYPATH
-sed -i $LDAP_USERNAME_PROP $SECURITYPATH
-sed -i $LDAP_PASSWORD_PROP $SECURITYPATH
-sed -i $GROUP_PREFIX_PROP $SECURITYPATH
-sed -i $CHANNEL_TYPE_PROP $SECURITYPATH
-sed -i $SECURE_CHANNEL_TYPE_PROP $SECURITYPATH
-sed -i $REALM_STRING_PROP $SECURITYPATH
-sed -i $JDBCURL_PROP $JDBCPATH
-sed -i $DBSCHEMA_PROP $JDBCPATH
-
-# Repackage the jar and war
-cd $SETTINGSPATH
-zip -r $SETTINGSJARPATH *
-mkdir $RESULTPATH
-cp -r $ROOTPATH/* $RESULTPATH
-
-
-# Start up tomcat
- /bin/bash /usr/local/tomcat/bin/catalina.sh run
+exec "$@"
